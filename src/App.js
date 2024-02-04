edfdc1d67df53b8dd1e38b0d59bee2cc9b5b8df1
hastook <147938852+Squiggly7525@users.noreply.github.com>
2024-02-04 22:02:36 +0330

    deleted sth in app.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Weather />
-        </a>
-      </header>
+      <Weather />
       <p>This project was coded by Hasti Salehi</p>
     </div>
   );
