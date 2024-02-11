6a484097af7a28f73de8511c9563a6996974c6f6
hastook <147938852+Squiggly7525@users.noreply.github.com>
2024-02-11 16:52:15 +0330

    Weather.css changes

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,30 +6,32 @@ import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo({ city, weatherDetails }) {
   return (
-    <div className="weather-info">
-      {" "}
-      <div className="main-humidity-wind">
-        <div className="main-data">
-          <h1>{city.toUpperCase()} </h1>{" "}
-          <FormattedDate date={weatherDetails.date} />
-          {", "}
-          <span className="description"> {weatherDetails.description}</span>
-        </div>{" "}
-        <div className="humidity-wind">
-          <span>Humidity: {weatherDetails.humidity}%</span>
-          {", "}
-          <span>Wind: {weatherDetails.wind} km/h</span>
-        </div>
-      </div>
-      <div className="icon-temperature-realfeel">
-        <div className="icon-and-temperature">
-          <WeatherIcon code={weatherDetails.icon} size={52} />
-          <img src={weatherDetails.iconUrl} />
-          <div className="temp">
-            <WeatherTemperature celsius={weatherDetails.temperature} />
+    <div className="weather-details-container">
+      <div className="weather-info">
+        {" "}
+        <div className="main-humidity-wind">
+          <div className="main-data">
+            <h1>{city.toUpperCase()} </h1>{" "}
+            <FormattedDate date={weatherDetails.date} />
+            {", "}
+            <span className="description"> {weatherDetails.description}</span>
+          </div>{" "}
+          <div className="humidity-wind">
+            <span>Humidity: {weatherDetails.humidity}%</span>
+            {", "}
+            <span>Wind: {weatherDetails.wind} km/h</span>
           </div>
         </div>
-        <span>Feels like: {weatherDetails.feelsLike}°C</span>
+        <div className="icon-temperature-realfeel">
+          <div className="icon-and-temperature">
+            <WeatherIcon code={weatherDetails.icon} size={52} />
+            <img src={weatherDetails.iconUrl} />
+            <div className="temp">
+              <WeatherTemperature celsius={weatherDetails.temperature} />
+            </div>
+          </div>
+          <span>Feels like: {weatherDetails.feelsLike}°C</span>
+        </div>
       </div>
     </div>
   );
